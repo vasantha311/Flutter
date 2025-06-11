597fe32f6be9bb03cf2a1108282ee082b8f4e632
unknown <vasantha.sanchi111@gmail.com>
2025-06-11 09:52:45 -0700

    program

--- signinpage/android/app/src/main/kotlin/com/example/signinpage/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.signinpage
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- signuppage/android/app/src/main/kotlin/com/example/signuppage/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.signuppage
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
